34f97358d644166fd8cee5b2cdc359f13e0a98c9
Affan <affank1020@gmail.com>
2024-08-02 13:10:08 +0100

    Added new colours for code bits

--- snipboard-web-app/src/components/Snip.js
+++ snipboard-web-app/src/components/Snip.js
@@ -24,7 +24,7 @@ const Snip = ({ title, language, code, description }) => {
         <span className="text-white font-bold pb-2">{title}</span>
         <button 
           onClick={handleCopy} 
-          className="absolute top-4 right-4 bg-[#1E1E1E] hover:bg-[#121212] text-white text-sm px-4 py-2 rounded flex items-center justify-center" 
+          className="absolute top-4 right-4 bg-codeBg hover:bg-darkCodeBg text-white text-sm px-4 py-2 rounded flex items-center justify-center" 
           style={{ width: '100px' }}
         >
           {copied ? <FaCheck className="mr-2" /> : <FaCopy className="mr-2" />}
@@ -37,15 +37,15 @@ const Snip = ({ title, language, code, description }) => {
       <div className="flex justify-between items-center mt-2">
         <span className="text-white px-4 py-2 rounded text-xs font-bold">{language}</span>
         <div className="flex space-x-2">
-          <button onClick={toggleExpanded} className="bg-[#1E1E1E] hover:bg-[#121212] text-white text-sm px-4 py-2 rounded flex items-center">
+          <button onClick={toggleExpanded} className="bg-codeBg hover:bg-darkCodeBg text-white text-sm px-4 py-2 rounded flex items-center">
             <FaBars className="mr-2" /> {expanded ? 'Less' : 'More'}
           </button>
-          <button className="bg-[#1E1E1E] hover:bg-[#121212] text-white text-sm px-4 py-2 rounded flex items-center">
+          <button className="bg-codeBg hover:bg-darkCodeBg text-white text-sm px-4 py-2 rounded flex items-center">
             <FaEdit className="mr-2" /> Edit
           </button>
         </div>
       </div>
-      <div className={`mt-2 text-white bg-[#2E2E2E] rounded-lg description ${expanded ? 'expanded' : ''}`}>
+      <div className={`mt-2 text-white bg-lightCodeBg rounded-lg description ${expanded ? 'expanded' : ''}`}>
         <h3 className="font-bold mb-2">Description</h3>
         {description}
       </div>
--- snipboard-web-app/tailwind.config.js
+++ snipboard-web-app/tailwind.config.js
@@ -14,7 +14,10 @@ module.exports = {
         blue: "#7BB7E0",
         lightBlue: "#DDE9F1",
         lightRed: "#FF4C4C",
-        darkRed: "#560216"
+        darkRed: "#560216",
+        codeBg: "#1E1E1E",
+        darkCodeBg: "#121212",
+        lightCodeBg: "#2E2E2E"
       }
     },
   },
